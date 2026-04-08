be7e66eb92ae261e1bcfe71120263aceaf2478c1
blaise789 <bigirabagaboblaise@gmail.com>
2026-04-09 00:19:53 +0200

    ci: add debug info to workflow and switch to gp2 volumes for free tier compatibility

--- backend/terraform/modules/compute/main.tf
+++ backend/terraform/modules/compute/main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "app_server" {
 
   root_block_device {
     volume_size = 20
-    volume_type = "gp3"
+    volume_type = "gp2"
   }
 
   tags = { Name = "${var.project_name}-server" }
